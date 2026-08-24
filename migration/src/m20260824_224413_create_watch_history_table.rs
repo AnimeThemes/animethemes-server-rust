379e34066aa2a89917af9da1b958863f74038fa5
Kyrch <kyrchk5@gmail.com>
2026-08-24 19:47:06 -0300

    feat: watch history migration

---
+++ migration/src/m20260824_224413_create_watch_history_table.rs
@@ -0,0 +1,73 @@
+use sea_orm_migration::prelude::*;
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m20260824_224413_create_watch_history_table"
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table("watch_history")
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new("id")
+                            .big_unsigned()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new("entry_id").big_unsigned().not_null())
+                    .col(ColumnDef::new("video_id").big_unsigned().not_null())
+                    .col(ColumnDef::new("user_id").big_unsigned().not_null())
+                    .col(
+                        ColumnDef::new("created_at")
+                            .timestamp()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .col(
+                        ColumnDef::new("updated_at")
+                            .timestamp()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("watch_history_entry_id_foreign")
+                            .from("watch_history", "entry_id")
+                            .to("anime_theme_entries", "entry_id")
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("watch_history_video_id_foreign")
+                            .from("watch_history", "video_id")
+                            .to("videos", "video_id")
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("watch_history_user_id_foreign")
+                            .from("watch_history", "user_id")
+                            .to("users", "id")
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+    }
+}
